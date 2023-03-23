8d05c788b090e5e92674d8c0058991e333acb818
anurag soni <soni.anurag0123@gmail.com>
2023-03-23 21:08:06 +0530

    Logging and first layout of button

--- app/src/main/java/com/example/sharemsgapp/MainActivity.kt
+++ app/src/main/java/com/example/sharemsgapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.sharemsgapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnShowToast : Button = findViewById<Button>(R.id.button)
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i("MainActivity", "Button was Clicked");
+
+            Toast.makeText(this, "Button was Clicked!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
